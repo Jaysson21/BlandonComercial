a3bb12f7941345ab03aad0c6f37865a8950243c9
Jaysson Fley González <57198318+Jaysson21@users.noreply.github.com>
2024-10-29 14:45:50 -0600

    Update BDVenta.sql

--- scripts/BDVenta.sql
+++ scripts/BDVenta.sql
@@ -4,71 +4,72 @@ CREATE DATABASE control_ventas_deudas;
 
 -- Tabla: Clientes
 CREATE TABLE Clientes (
-    cliente_id SERIAL PRIMARY KEY,
+    clienteid SERIAL PRIMARY KEY,
     nombres VARCHAR(50) NOT NULL,
     apellidos VARCHAR(50) NOT NULL,
     telefono VARCHAR(20),
     email VARCHAR(50),
     direccion VARCHAR(250),
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecharegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabla: Productos
 CREATE TABLE Productos (
-    producto_id SERIAL PRIMARY KEY,
+    productoid SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     descripcion VARCHAR(50)
 );
 
 -- Tabla: Usuarios
 CREATE TABLE Usuarios (
-    usuario_id SERIAL PRIMARY KEY,
-    nombre_usuario VARCHAR(50),
+    usuarioid SERIAL PRIMARY KEY,
+    nombreusuario VARCHAR(50),
     clave TEXT,
-    estado_usuario BOOLEAN,
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    estadousuario BOOLEAN,
+    fechacreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabla: Ventas
 CREATE TABLE Ventas (
-    venta_id SERIAL PRIMARY KEY,
-    cliente_id INT,
-    usuario_id INT,
-    fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    tipo_venta VARCHAR(10) DEFAULT 'Pendiente',
+    ventaid SERIAL PRIMARY KEY,
+    clienteid INT,
+    usuarioid INT,
+    fechaventa TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tipoventa VARCHAR(10) DEFAULT 'Pendiente',
     observacion VARCHAR(300) DEFAULT '',
-    CONSTRAINT fk_ventas_clientes FOREIGN KEY (cliente_id) REFERENCES Clientes(cliente_id),
-    CONSTRAINT fk_ventas_usuarios FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuario_id)
+    CONSTRAINT fk_ventas_clientes FOREIGN KEY (clienteid) REFERENCES Clientes(clienteid),
+    CONSTRAINT fk_ventas_usuarios FOREIGN KEY (usuarioid) REFERENCES Usuarios(usuarioid)
 );
 
 -- Tabla: DetalleVentas
 CREATE TABLE DetalleVentas (
-    detalle_venta_id SERIAL PRIMARY KEY,
-    venta_id INT,
-    producto_id INT,
+    detalleventaid SERIAL PRIMARY KEY,
+    ventaid INT,
+    productoid INT,
     cantidad INT NOT NULL,
-    precio_unitario DECIMAL(10, 2) NOT NULL,
-    CONSTRAINT fk_detalle_ventas_ventas FOREIGN KEY (venta_id) REFERENCES Ventas(venta_id),
-    CONSTRAINT fk_detalle_ventas_productos FOREIGN KEY (producto_id) REFERENCES Productos(producto_id)
+    preciounitario DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT fk_detalle_ventas_ventas FOREIGN KEY (ventaid) REFERENCES Ventas(ventaid),
+    CONSTRAINT fk_detalle_ventas_productos FOREIGN KEY (productoid) REFERENCES Productos(productoid)
 );
 
 -- Tabla: Deudas
 CREATE TABLE Deudas (
-    deuda_id SERIAL PRIMARY KEY,
-    cliente_id INT,
-    venta_id INT,
-    monto_deuda DECIMAL(10, 2) NOT NULL,
-    estado_deudas VARCHAR(10) DEFAULT 'Pendiente',
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_deudas_clientes FOREIGN KEY (cliente_id) REFERENCES Clientes(cliente_id),
-    CONSTRAINT fk_deudas_ventas FOREIGN KEY (venta_id) REFERENCES Ventas(venta_id)
+    deudaid SERIAL PRIMARY KEY,
+    clienteid INT,
+    ventaid INT,
+    montodeuda DECIMAL(10, 2) NOT NULL,
+    estadodeudas VARCHAR(10) DEFAULT 'Pendiente',
+    fechacreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_deudas_clientes FOREIGN KEY (clienteid) REFERENCES Clientes(clienteid),
+    CONSTRAINT fk_deudas_ventas FOREIGN KEY (ventaid) REFERENCES Ventas(ventaid)
 );
 
 -- Tabla: Pagos
 CREATE TABLE Pagos (
-    pago_id SERIAL PRIMARY KEY,
-    deuda_id INT,
-    monto_abono DECIMAL(10, 2) NOT NULL,
-    fecha_pago TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_pagos_deudas FOREIGN KEY (deuda_id) REFERENCES Deudas(deuda_id)
+    pagoid SERIAL PRIMARY KEY,
+    deudaid INT,
+    montoabono DECIMAL(10, 2) NOT NULL,
+    fechapago TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tipopago character varying(10) COLLATE pg_catalog."default" DEFAULT 'Parcial'::character varying,
+    CONSTRAINT fk_pagos_deudas FOREIGN KEY (deudaid) REFERENCES Deudas(deudaid)
 );
